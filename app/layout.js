704b4f84e0b5a4281b8adb2e6da0fed4336b1ef5
Leonhard Hoffmann <Leonhard.hoffmann8@gmail.com>
2025-12-26 15:52:43 +0100

    style: set roboto as default font

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from 'next/font/google';
 import "./globals.css";
 import Navbar from "./components/Navbar.js";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const roboto = Roboto({
+  subsets: ['latin'],            // 'latin' is standard
+  display: 'swap',               // Recommended for performance
 });
 
 // METADATA API (Next.js 13+)
@@ -50,7 +45,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased md:ml-56 pt-14 md:pt-0`}
+        className={`${roboto.className} antialiased md:ml-56 pt-14 md:pt-0`}
       >
         <Navbar />
         {children}
